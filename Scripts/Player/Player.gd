413cf336a5aac29be21cb6226dd44138ecca2140
naixsu <plnacua02@gmail.com>
2023-11-10 23:12:10 +0800

    Initial player movement

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -3,24 +3,52 @@ extends CharacterBody2D
 # Export vars here
 @export var speed = 300
 
+# Onready vars here
+@onready var anim = $AnimatedSprite2D
+
 # Other global vars here
+var dead = false
+
+func _ready():
+	anim.play("idle")
+	
 
 func _physics_process(delta):
-	
 	var direction = Input.get_vector("Left", "Right", "Up", "Down")
-	
-	
-	update_gun_rotation()
+
 	if Input.is_action_just_pressed("Fire"):
 		fire()
 	
-	
 	velocity = direction * speed
-	move_and_slide()
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		dead = not dead
+		anim.play("death")
+		
+	if not dead:
+		update_gun_rotation()
+		move_and_slide()
+		update_animation()
+	
 
 func update_gun_rotation():
 	# Rotates the gun arrow according to the mouse position
 	$GunRotation.look_at(get_global_mouse_position())
+
+func update_animation():
+	flip_sprite()
+	# Updates player animation based on velocity
+	if velocity != Vector2.ZERO:
+		anim.play("run")
+	else:
+		anim.play("idle")
+
+func flip_sprite():
+	# Flipts the sprite depending on the mouse position
+	if get_global_mouse_position().x < global_position.x:
+		anim.flip_h = true
+	elif get_global_mouse_position().x > global_position.x:
+		anim.flip_h = false
 
 func fire():
 	# Add bullet methods here

5f388e49af1772f57029d4b199ae1f78a6880f82
naixsu <plnacua02@gmail.com>
2023-11-11 15:40:44 +0800

    Player bullet and shooting

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -2,12 +2,19 @@ extends CharacterBody2D
 
 # Export vars here
 @export var speed = 300
+@export var Bullet : PackedScene
 
 # Onready vars here
 @onready var anim = $AnimatedSprite2D
+@onready var gun_rotation = $GunRotation
+@onready var fire_cooldown = $FireCooldown
+
+# Signals here
+signal player_fired_bullet(bullet, pos, dir)
 
 # Other global vars here
 var dead = false
+var health = 100
 
 func _ready():
 	anim.play("idle")
@@ -15,9 +22,6 @@ func _ready():
 
 func _physics_process(delta):
 	var direction = Input.get_vector("Left", "Right", "Up", "Down")
-
-	if Input.is_action_just_pressed("Fire"):
-		fire()
 	
 	velocity = direction * speed
 	
@@ -29,11 +33,15 @@ func _physics_process(delta):
 		update_gun_rotation()
 		move_and_slide()
 		update_animation()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("Fire"):
+		fire()
 	
 
 func update_gun_rotation():
 	# Rotates the gun arrow according to the mouse position
-	$GunRotation.look_at(get_global_mouse_position())
+	gun_rotation.look_at(get_global_mouse_position())
 
 func update_animation():
 	flip_sprite()
@@ -51,5 +59,29 @@ func flip_sprite():
 		anim.flip_h = false
 
 func fire():
-	# Add bullet methods here
-	print("Fire")
+	if fire_cooldown.is_stopped():
+		var b = Bullet.instantiate()
+	#	b.global_position = gun_rotation.get_node("BulletSpawn").global_position
+	#	b.rotation_degrees = gun_rotation.rotation_degrees
+	#
+	#	# Set direction of bullet
+	#	var target = get_global_mouse_position()
+	#	var direction_to_mouse = b.global_position.direction_to(target).normalized()
+	#	b.set_direction(direction_to_mouse)
+		
+		# Add bullet to the tree
+	#	get_tree().root.add_child(b)
+		var target = get_global_mouse_position()
+		var pos = gun_rotation.get_node("BulletSpawn").global_position
+		var direction_to_mouse = pos.direction_to(target).normalized()
+		emit_signal("player_fired_bullet", b, pos, direction_to_mouse)
+		fire_cooldown.start()
+
+
+func handle_hit():
+	health -= 20
+	print("Player hit", health)
+	
+	
+	
+	
---
+++ Scripts/Testing/NaixTestScripts/NaixTestScene.gd
@@ -0,0 +1,9 @@
+extends Node
+
+@onready var bullet_manager = $BulletManager
+@onready var player = $Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player.connect("player_fired_bullet", bullet_manager.handle_bullet_spawned)
+

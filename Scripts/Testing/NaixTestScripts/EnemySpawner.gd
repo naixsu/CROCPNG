27c49f95ceb0b549ea3296c031fd2a213048f01a
naixsu <plnacua02@gmail.com>
2023-11-23 22:40:42 +0800

    Updated variable names

--- Scripts/Testing/NaixTestScripts/EnemySpawner.gd
+++ Scripts/Testing/NaixTestScripts/EnemySpawner.gd
@@ -10,22 +10,22 @@ func _spawn_enemy(data):
 		print(data)
 		return null
 #	var bomb = preload("res://bomb.tscn").instantiate()
-	var enemy_type = data[1]
+	var enemyType = data[1]
 #	var enemy = null
-	var enemy_scene_path = ""
+	var enemyScenePath = ""
 #	if enemy_type == "A": enemy = preload("res://Scenes/Enemies/EnemyA/EnemyA.tscn").instantiate()
 #	elif enemy_type == "B": enemy = preload("res://Scenes/Enemies/EnemyB/EnemyB.tscn").instantiate()
 #	elif enemy_type == "C": enemy = preload("res://Scenes/Enemies/EnemyC/EnemyC.tscn").instantiate()
-	match enemy_type:
+	match enemyType:
 		"A":
-			enemy_scene_path = "res://Scenes/Enemies/EnemyA/EnemyA.tscn"
+			enemyScenePath = "res://Scenes/Enemies/EnemyA/EnemyA.tscn"
 		"B":
-			enemy_scene_path = "res://Scenes/Enemies/EnemyB/EnemyB.tscn"
+			enemyScenePath = "res://Scenes/Enemies/EnemyB/EnemyB.tscn"
 		"C":
-			enemy_scene_path = "res://Scenes/Enemies/EnemyC/EnemyC.tscn"
-	var enemy = load(enemy_scene_path).instantiate()
+			enemyScenePath = "res://Scenes/Enemies/EnemyC/EnemyC.tscn"
+	var enemy = load(enemyScenePath).instantiate()
 	enemy.position = data[0]
 	
-	print("Enemey " + enemy_type + " spawned at " + str(enemy.position))
+	print("Enemey " + enemyType + " spawned at " + str(enemy.position))
 
 	return enemy

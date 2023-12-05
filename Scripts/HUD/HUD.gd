7e7677260f46dfa2b4aa785a19ca7e655b9efa7b
naixsu <plnacua02@gmail.com>
2023-12-05 10:59:09 +0800

    Hotbar

--- Scripts/HUD/HUD.gd
+++ Scripts/HUD/HUD.gd
@@ -1,6 +1,11 @@
 extends CanvasLayer
 
 
+@onready var healthBar = $HealthBar
+@onready var playerIcon = $PlayerIcon
+@onready var hotBar = $HotBar
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
---
+++ Scripts/HUD/HotBar.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+@onready var pistolButton = $GridContainer/PistolButton
+@onready var rifleButton = $GridContainer/RifleButton
+@onready var shotgunButton = $GridContainer/ShotgunButton
+@onready var meleeButton = $GridContainer/MeleeButton

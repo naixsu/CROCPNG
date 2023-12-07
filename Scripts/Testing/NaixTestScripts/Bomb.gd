9ff128e9e3f905202ab85701ea448a87a5788596
naixsu <plnacua02@gmail.com>
2023-12-07 22:33:10 +0800

    Bomb sound

--- Scripts/Testing/NaixTestScripts/Bomb.gd
+++ Scripts/Testing/NaixTestScripts/Bomb.gd
@@ -3,6 +3,7 @@ extends Node2D
 var isHeld : bool = false
 
 @onready var anim = $AnimatedSprite2D
+@onready var SoundManager = $SoundManager
 
 func _ready():
 	anim.play("detonate")
@@ -13,3 +14,7 @@ func _on_animated_sprite_2d_animation_finished():
 		var root = get_tree().get_root()
 		var multScene = root.get_node("TestMultiplayerScene")
 		multScene.lose.rpc()
+
+func _on_animated_sprite_2d_frame_changed():
+	if anim.frame == 14:
+		SoundManager.bomb.play()

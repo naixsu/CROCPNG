f954f49e54bd0722eea65fe889611be0ad6e9c46
yong-01 <120231707+yong-01@users.noreply.github.com>
2023-12-09 15:16:42 +0800

    updated

--- Scripts/HUD/HUD.gd
+++ Scripts/HUD/HUD.gd
@@ -1,11 +1,11 @@
 extends CanvasLayer
 
 
-@onready var healthBar = $HealthBar
+@onready var healthBar = $PlayerStats/HealthAndMoney/Stats/Health/HealthBar
 @onready var playerIcon = $PlayerIcon
 @onready var hotBar = $HotBar
-@onready var moneyAnim = $MoneyAnim
-@onready var moneyText = $MoneyText
+@onready var moneyAnim = $PlayerStats/HealthAndMoney/Stats/Money/MoneyAnim
+@onready var moneyText = $PlayerStats/HealthAndMoney/Stats/Money/MoneyText
 @onready var pistolButton = $HotBar/HBoxContainer/PistolButton
 @onready var rifleButton = $HotBar/HBoxContainer/RifleButton
 @onready var shotgunButton = $HotBar/HBoxContainer/ShotgunButton
